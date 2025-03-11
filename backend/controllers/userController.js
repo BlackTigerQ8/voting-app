558d98d11e2fbeec5de4ccd1933ec766730abf8a
blacktigerq8 <blacktigerq8@gmail.com>
2025-03-12 02:17:28 +0300

    Completed User CRUD in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,6 @@ const { User } = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const {
-  sendVerificationEmail,
-  sendContactFormEmail,
-} = require("../emailService");
 
 // @desc    Get all users
 // @route   GET /api/users
@@ -15,42 +11,23 @@ const getAllusers = async (req, res) => {
     let query = {};
     const { role } = req.query;
 
-    // If user is a coach, only return their assigned athletes
-    if (req.user.role === "Coach") {
-      query = {
-        $or: [
-          { _id: req.user._id }, // Include the coach themselves
-          { role: "Athlete", coach: req.user._id }, // Include their athletes
-        ],
-      };
-    } else if (req.user.role === "Admin") {
+    if (req.user.role === "Admin" || req.user.role === "SuperAdmin") {
       // If a specific role is requested (e.g., for coach selection)
       if (req.query.role) {
         query.role = req.query.role;
       }
-    } else if (req.user.role === "Athlete" || req.user.role === "Family") {
-      // Allow Athletes and Family members to see only their own profile
+    } else if (req.user.role === "Voter") {
+      // Allow customers to see only their own profile
       query = { _id: req.user._id };
     } else {
-      // If user is neither Admin nor Coach, return unauthorized
+      // If user is neither Admin nor Barista, return unauthorized
       return res.status(403).json({
         status: "Error",
         message: "Not authorized to access user list",
       });
     }
 
-    // If user is neither Admin nor Coach, return unauthorized
-    // if (req.user.role !== "Admin" && req.user.role !== "Coach") {
-    //   return res.status(403).json({
-    //     status: "Error",
-    //     message: "Not authorized to access user list",
-    //   });
-    // }
-
-    const users = await User.find(query).populate(
-      "coach",
-      "firstName lastName _id role"
-    );
+    const users = await User.find(query);
 
     res.status(200).json({
       status: "Success",
@@ -82,7 +59,11 @@ const getUser = async (req, res) => {
     }
 
     // Check if the user is accessing their own data or is an admin
-    if (req.user.id !== req.params.id && req.user.role !== "Admin") {
+    if (
+      req.user.id !== req.params.id &&
+      req.user.role !== "Admin" &&
+      req.user.role !== "SuperAdmin"
+    ) {
       return res.status(403).json({
         status: "Error",
         message: "You do not have permission to access this user's data",
@@ -91,7 +72,7 @@ const getUser = async (req, res) => {
 
     res.status(200).json({
       stauts: "Success",
-      data: { ...user._doc, imagePath: user.image },
+      data: { ...user._doc, idImagePath: user.idImage },
     });
   } catch (error) {
     res.status(500).json({
@@ -106,71 +87,56 @@ const getUser = async (req, res) => {
 // @access  Private/Admin
 const createUser = async (req, res) => {
   try {
-    const uploadedFile = req.file;
-    const filePath = uploadedFile ? uploadedFile.path : null;
-    const newUser = await User.create({ ...req.body, image: filePath });
+    // If someone is logged in (admin), they can set any role
+    if (
+      req.user &&
+      (req.user.role === "Admin" || req.user.role === "SuperAdmin")
+    ) {
+      // Admin can create any type of user
+      const uploadedFile = req.file;
+      const filePath = uploadedFile ? uploadedFile.path : null;
+      const newUser = await User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+        idNumber: req.body.idNumber,
+        role: req.body.role, // Admin can set any role
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        idImage: filePath,
+      });
 
-    // Generate verification token
-    const verificationToken = newUser.createEmailVerificationToken();
-    await newUser.save({ validateBeforeSave: false });
+      await newUser.save({ validateBeforeSave: false });
 
-    // Create verification URL with explicit frontend URL
-    if (!process.env.FRONTEND_URL) {
-      console.error("FRONTEND_URL is not defined in environment variables");
-    }
-
-    const verificationUrl = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`;
-    console.log("Verification URL:", verificationUrl); // For debugging
-
-    // Send verification email
-    await sendVerificationEmail(newUser.email, verificationUrl);
-
-    res.status(201).json({
-      status: "Success",
-      data: {
-        user: newUser,
-      },
-    });
-  } catch (error) {
-    res.status(500).json({
-      status: "Error",
-      message: error.message,
-    });
-  }
-};
-
-// Verification endpoint
-const verifyEmail = async (req, res) => {
-  try {
-    const hashedToken = crypto
-      .createHash("sha256")
-      .update(req.params.token)
-      .digest("hex");
-
-    const user = await User.findOne({
-      emailVerificationToken: hashedToken,
-      emailVerificationExpires: { $gt: Date.now() },
-    });
-
-    if (!user) {
-      return res.status(400).json({
-        status: "Error",
-        message: "Token is invalid or has expired",
+      return res.status(201).json({
+        status: "Success",
+        data: { user: newUser },
       });
     }
 
-    // Update user verification status
-    user.isEmailVerified = true;
-    user.emailVerificationToken = undefined;
-    user.emailVerificationExpires = undefined;
-    await user.save({ validateBeforeSave: false });
+    // For public registration, force role to be "Voter"
+    const uploadedFile = req.file;
+    const filePath = uploadedFile ? uploadedFile.path : null;
+    const newUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      idNumber: req.body.idNumber,
+      role: "Voter", // Force role to be Voter for public registration
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      idImage: filePath,
+    });
 
-    res.status(200).json({
+    await newUser.save({ validateBeforeSave: false });
+
+    res.status(201).json({
       status: "Success",
-      message: "Email verified successfully",
+      data: { user: newUser },
     });
   } catch (error) {
-    console.error("Verification error:", error);
     res.status(500).json({
       status: "Error",
       message: error.message,
@@ -184,7 +150,11 @@ const verifyEmail = async (req, res) => {
 const updateUser = async (req, res) => {
   try {
     // Check if the user is accessing their own data or is an admin
-    if (req.user.id !== req.params.id && req.user.role !== "Admin") {
+    if (
+      req.user.id !== req.params.id &&
+      req.user.role !== "Admin" &&
+      req.user.role !== "SuperAdmin"
+    ) {
       return res.status(403).json({
         status: "Error",
         message: "You do not have permission to access this user's data",
@@ -196,50 +166,14 @@ const updateUser = async (req, res) => {
 
     // Prepare the updated data
     const updateData = req.file
-      ? { ...req.body, image: filePath }
+      ? { ...req.body, idImage: filePath }
       : { ...req.body };
-
-    // Handle coach field based on role
-    if (updateData.role === "Athlete") {
-      if (!updateData.coach) {
-        return res.status(400).json({
-          status: "Error",
-          message: "Coach is required for athletes",
-        });
-      }
-    } else {
-      // If role is not Athlete, remove coach field
-      updateData.coach = undefined;
-    }
 
     // Check if password is included in the request body and hash it
     if (req.body.password) {
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(req.body.password, salt);
       updateData.password = hashedPassword;
-    }
-
-    // Get the current user data
-    const currentUser = await User.findById(req.params.id);
-
-    // Check if email is being updated
-    if (updateData.email && updateData.email !== currentUser.email) {
-      // Set email verification status to false
-      updateData.isEmailVerified = false;
-
-      // Generate new verification token
-      const verificationToken = crypto.randomBytes(32).toString("hex");
-      updateData.emailVerificationToken = crypto
-        .createHash("sha256")
-        .update(verificationToken)
-        .digest("hex");
-      updateData.emailVerificationExpires = Date.now() + 24 * 60 * 60 * 1000; // 24 hours
-
-      // Create verification URL
-      const verificationUrl = `${process.env.FRONTEND_URL}/verify-email/${verificationToken}`;
-
-      // Send verification email
-      await sendVerificationEmail(updateData.email, verificationUrl);
     }
 
     const user = await User.findByIdAndUpdate(req.params.id, updateData, {
@@ -283,11 +217,23 @@ const deleteUser = async (req, res) => {
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  const { emailOrPhone, password } = req.body;
+
+  if (!emailOrPhone || !password) {
+    return res.status(400).json({
+      status: "Error",
+      message: "Please provide email/phone and password",
+    });
+  }
 
   try {
-    // Check for user email
-    const user = await User.findOne({ email });
+    // Check if input is email or phone
+    const isEmail = emailOrPhone.includes("@");
+
+    // Find user by either email or phone
+    const user = await User.findOne(
+      isEmail ? { email: emailOrPhone } : { phone: emailOrPhone }
+    );
 
     if (!user) {
       return res.status(401).json({
@@ -316,8 +262,9 @@ const loginUser = async (req, res) => {
       email: userEmail,
       _id,
       role,
-      image,
+      idImage,
       phone,
+      idNumber,
     } = userObj;
 
     // Create token
@@ -335,8 +282,9 @@ const loginUser = async (req, res) => {
           email: userEmail,
           _id,
           role,
-          image,
+          idImage,
           phone,
+          idNumber,
         },
       },
     });
@@ -366,10 +314,10 @@ const logoutUser = (req, res) => {
 // @access  Public
 const contactMessage = async (req, res) => {
   try {
-    const { firstName, lastName, email, phone, message } = req.body;
+    const { firstName, lastName, email, phone, message, idNumber } = req.body;
 
     // Basic validation
-    if (!firstName || !lastName || !email || !phone || !message) {
+    if (!firstName || !lastName || !email || !phone || !message || !idNumber) {
       return res.status(400).json({
         status: "Error",
         message: "All fields are required",
@@ -383,6 +331,7 @@ const contactMessage = async (req, res) => {
       email,
       phone,
       message,
+      idNumber,
     });
 
     // Send success response
@@ -406,6 +355,5 @@ module.exports = {
   deleteUser,
   loginUser,
   logoutUser,
-  verifyEmail,
   contactMessage,
 };
